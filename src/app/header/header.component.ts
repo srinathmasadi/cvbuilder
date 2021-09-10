7ca42d9cfe73b8a26fe0b061ada7700a9b2f7727
srinathmasadi <mr.masadi25@gmail.com>
2021-09-10 16:53:44 +0530

    Updated the home page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css','../home/home.component.css']
 })
 export class HeaderComponent implements OnInit {
 
